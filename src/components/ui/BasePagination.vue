3bf319fbe7f4c5d2d1847493e0b124ac4b9c9f81
Yash Verma <yash.silver2110@gmail.com>
2024-12-06 00:55:42 +0530

    feat: added pagination molecule

---
+++ src/components/ui/BasePagination.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="pagination">
+    <BaseButton
+      :disabled="props.pageNumber === 1"
+      rounded="full"
+      type="outlined"
+      variant="accent"
+      @click="emit('previous')"
+    >
+      <FontAwesomeIcon :icon="faArrowLeft" />
+    </BaseButton>
+
+    <!-- List of pages -->
+    <ul class="pagination__pages">
+      <li
+        v-for="page in listOfPages"
+        :key="page"
+      >
+        <BaseButton
+          :disabled="page === '...'"
+          variant="white"
+          @click="handlePageClick(page)"
+        >
+          <BaseTypography
+            size="sm"
+            tag="span"
+            :weight="page === props.pageNumber ? 'bold' : 'regular'"
+          >
+            {{ page === '...' ? '...' : page }}
+          </BaseTypography>
+        </BaseButton>
+      </li>
+    </ul>
+
+    <BaseButton
+      :disabled="props.pageNumber === totalPages"
+      rounded="full"
+      type="outlined"
+      variant="accent"
+      @click="emit('next')"
+    >
+      <FontAwesomeIcon :icon="faArrowRight" />
+    </BaseButton>
+
+    <BaseDropdown
+      direction="top"
+      :options="getRecordsPerPageOptions()"
+      size="sm"
+      title="Records per page"
+      @select="handleRecordsPerPageChange"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseTypography from '@/components/ui/BaseTypography.vue'
+import BaseButton from '@/components/ui/BaseButton.vue'
+import BaseDropdown from '@/components/ui/BaseDropdown.vue'
+import { computed } from 'vue'
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+interface IPaginationProps {
+  recordsPerPage: number
+  totalRecords: number
+  pageNumber: number
+}
+const props = defineProps<IPaginationProps>()
+
+const emit = defineEmits<{
+  (e: 'next'): void
+  (e: 'previous'): void
+  (e: 'page-click', page: number): void
+  (e: 'records-per-page-change', recordsPerPage: number): void
+}>()
+
+const recordsPerPageOptions = [10, 20, 30, 40, 50]
+
+const totalPages = computed(() => Math.ceil(props.totalRecords / props.recordsPerPage))
+
+const listOfPages = computed(() => {
+  const pageList: (number | string)[] = []
+
+  if (props.pageNumber > 3) pageList.push(1)
+
+  if (props.pageNumber > 4) {
+    pageList.push('...')
+  }
+
+  const start = Math.max(1, props.pageNumber - 1)
+  const end = Math.min(totalPages.value, props.pageNumber + 1)
+  for (let page = start; page <= end; page++) {
+    pageList.push(page)
+  }
+
+  if (props.pageNumber < totalPages.value - 3) {
+    pageList.push('...')
+  }
+
+  if (props.pageNumber < totalPages.value - 2) {
+    pageList.push(totalPages.value)
+  }
+
+  return pageList
+})
+
+function getRecordsPerPageOptions() {
+  return recordsPerPageOptions.map((option) => ({
+    label: option,
+    value: option,
+  }))
+}
+
+function handlePageClick(page: number | string) {
+  if (typeof page === 'number') {
+    emit('page-click', page)
+  }
+}
+
+function handleRecordsPerPageChange(recordsPerPage: number) {
+  emit('records-per-page-change', recordsPerPage)
+}
+</script>
+
+<style scoped lang="scss">
+.pagination {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.pagination__pages {
+  display: flex;
+  align-items: center;
+  list-style: none;
+  padding-inline-start: 0;
+}
+</style>
